e79a453f59c69606ff8b7d78e79170d32ceea496
manoj kumar <manoj.sanam2002@gmail.com>
2024-02-28 17:48:12 +0530

    Django react realestate frontend

--- django-react/backend/crud/settings.py
+++ django-react/backend/crud/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zvc*d9n^7*w^d9sqw3vj-92(89oco(n^#hn0a-3^v#bt#+ba*&'
+SECRET_KEY = 'secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
